84184e0d30910c052c2be1e871624f4c2bb239b7
DmitriyVik <62546488+DmitriyVik@users.noreply.github.com>
2020-06-06 13:44:29 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-$(document).ready(function () {
-  console.log('страница загрузилась');
+$(document).ready(function() {
+  console.log("страница загрузилась");
 
-  let tabsItem = $('.tabs-item');
+  let tabsItem = $(".tabs-item");
   console.log(tabsItem);
-  tabsItem.on('click', function(event){
+  tabsItem.on("click", function(event) {
     event.preventDefault();
-    let activeContent = $(this).attr('href');
-    $('.visible').toggleClass('visible');
-    $(activeContent).toggleClass('visible');
-    $('.tabs-item-active').toggleClass('tabs-item-active');
-    $(this).toggleClass('tabs-item-active')
-  })
+    let activeContent = $(this).attr("href");
+    $(".visible").toggleClass("visible");
+    $(activeContent).toggleClass("visible");
+    $(".tabs-item-active").toggleClass("tabs-item-active");
+    $(this).toggleClass("tabs-item-active");
+  });
 });
